809ce09ce241b2e1a9129634e6ec4e2e684a76e0
Mohamed Zaghlol <117047339+MohamedZaghlol0@users.noreply.github.com>
2024-12-10 23:00:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+     Solution s1 = new Solution();
+        s1.IsPalindrome(-121);
+    }
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            bool result;
+            if (x < 0)
+                return result = false;
+
+            char[] numstr = x.ToString().ToCharArray();
+            int L = numstr.Length;
+            char[] temp = new char[L];
+            for (int i =0 ; i <= L-1; i++) 
+            {
+                temp[L - i - 1] = numstr[i];
+            }
+            result = true;
+            for (int i = 0; i < L; i++)
+            {
+                if (numstr[i] != temp[i])
+                {
+                    result = false;
+                    break;
+                }
+            }
+            foreach (char a in numstr)
+            {
+                Console.WriteLine(a);
+            }
+            foreach (char b in temp)
+            {
+                Console.WriteLine(b);
+            }
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
