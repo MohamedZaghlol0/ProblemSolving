70beabe14dd322bf47cf197af44b51a748f1800c
mohamed zaghlol <mohamedzaghlol141@outlook.com>
2024-12-16 16:33:38 +0200

    #day4

---
+++ ArrayState/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] myarr = [2, 1, 3, 5, 6];
+        Solution solution = new Solution();
+        solution.GetFinalState(myarr,5,2);
+    }
+    public class Solution
+    {
+        public int[] GetFinalState(int[] nums, int k, int multiplier)
+        {
+            for (int j = 0; j < k; j++)
+            {
+                int minValue = nums[0];
+                int minIndex = 0;
+
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    if (nums[i] < minValue)
+                    {
+                        minValue = nums[i];
+                        minIndex = i;
+                    }
+                }
+                nums[minIndex] *= multiplier;
+            }
+
+            return nums;
+        }
+    }
+}
---
+++ EqualFrequencyletter/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        solution.EqualFrequency("abcc");
+    }
+    public class Solution
+    {
+        public bool EqualFrequency(string word)
+        {
+            bool result = true;
+            int repeated = 0;
+            int repindx = 0;
+            char[] words = word.ToCharArray();
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                int frq = 0;
+                
+                for (int j = 0; j < word.Length - 1; j++)
+                {
+                    if (words[i] == word[j])
+                    {
+                       frq += 1;
+                    }
+                    if (frq >= 2) 
+                    {
+                        result = false;
+
+                    }
+                }    
+
+            }
+            if (repeated == 1) 
+            {
+                word.Remove(repindx);
+            }
+            Console.WriteLine(word)    ;
+            return result;
+        }
+    }
+}
