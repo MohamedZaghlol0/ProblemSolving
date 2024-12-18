ec0dbf9fe9caf0404d84a21fc102769d3dc87e16
mohamed zaghlol <mohamedzaghlol141@outlook.com>
2024-12-18 12:18:45 +0200

    #day5

---
+++ FinalPrice/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        solution.FinalPrices([8, 4, 6, 2, 3]);
+    }
+    public class Solution
+    {
+        public int[] FinalPrices(int[] prices)
+        {
+            for (int i = 0; i < prices.Length; i++)
+            {
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    if (prices[j] <= prices[i])
+                    {
+                        prices[i] -= prices[j];
+                        break;
+                    }
+                }
+            }
+
+            foreach (var item in prices)
+            {
+                Console.WriteLine(item);
+            }
+            return prices;
+        }
+    }
+}
